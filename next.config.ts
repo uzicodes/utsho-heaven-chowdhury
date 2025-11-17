63a96908c3108f112aad1cfe345aa3295ca92c10
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-11-17 18:22:11 +0600

    upadetd the config file to use images.remotePatterns for placehold.co.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["placehold.co"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
   },
 };
 
