9d1608a5b33fe9ce8e80d2975af03c5a81dc7684
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-04-19 00:15:13 +0600

    feat: made the Name heading size a bit bigger for better visual ( just for desktop view, kept for mobile view as it is )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
                             backgroundClip: 'text',
                             WebkitBackgroundClip: 'text',
                             WebkitTextFillColor: 'transparent',
-                            fontSize: '45px',
+                            fontSize: '50px',
                             display: 'block',
                             marginTop: '0',
                             lineHeight: '1'
